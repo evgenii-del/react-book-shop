5c47cd216cce0aa42e9bd43036fd0595c67ff614
Khasanov <zkhasanovv@gmail.com>
2021-03-23 19:56:49 +0200

    Created filter by price

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -8,9 +8,38 @@ const Detail = () => {
   const { user, books } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [search, setSearch] = useState('');
+  const [select, setSelect] = useState('');
+
+  // eslint-disable-next-line consistent-return
+  const checkPrice = (book) => {
+    const { price } = book;
+    switch (select) {
+      case '1':
+        if (price < 25) {
+          return book;
+        }
+        break;
+      case '2':
+        if (price >= 25 && price <= 50) {
+          return book;
+        }
+        break;
+      case '3':
+        if (price > 50) {
+          return book;
+        }
+        break;
+      default:
+        return book;
+    }
+  };
 
   const handleChangeSearch = ({ target }) => {
     setSearch(target.value);
+  };
+
+  const handleChangeSelect = ({ target }) => {
+    setSelect(target.value);
   };
 
   useEffect(() => {
@@ -21,8 +50,14 @@ const Detail = () => {
     <div className="container">
       <Header />
       <nav className="nav">
+        <select className="nav__select" value={select} onChange={handleChangeSelect}>
+          <option value="0">all</option>
+          <option value="1">less then 25</option>
+          <option value="2">more then 25 less then 50</option>
+          <option value="3">more then 50</option>
+        </select>
         <input
-          className="nav__form-input"
+          className="nav__input"
           type="text"
           placeholder="Search..."
           value={search}
@@ -34,6 +69,7 @@ const Detail = () => {
         <div className="catalog__content">
           {books.data
             .filter((book) => book.title.toLowerCase().includes(search.toLowerCase().trim()))
+            .filter((book) => checkPrice(book))
             .map((book) => (
               <Card book={book} key={book.id} />
             ))}
