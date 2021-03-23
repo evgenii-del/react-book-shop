64be63d23bc6b0e924670bdfb0aeeeea12334fc5
Khasanov <zkhasanovv@gmail.com>
2021-03-23 17:44:15 +0200

    Created search field

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -8,11 +8,6 @@ import { logout } from '../redux/actions/user';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const [search, setSearch] = useState('');
-
-  const handleChangeSearch = ({ target }) => {
-    setSearch(target.value);
-  };
 
   const handleLogout = () => {
     dispatch(logout());
@@ -24,17 +19,6 @@ const Header = () => {
         Book Shop
       </Link>
       <div className="header__info">
-        <form className="header__form">
-          <input
-            className="header__form-input"
-            type="text"
-            value={search}
-            onChange={handleChangeSearch}
-          />
-          <button className="header__form-btn" type="button">
-            Search
-          </button>
-        </form>
         <div className="header__cart" onClick={handleLogout} aria-hidden>
           <img className="header__cart-image" src={logoutPng} alt="logout" />
           <span className="header__cart-label">Logout</span>
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Card, Header } from '../components';
@@ -7,6 +7,11 @@ import { getCalendarData } from '../redux/actions/catalog';
 const Detail = () => {
   const { user, books } = useSelector((state) => state);
   const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+
+  const handleChangeSearch = ({ target }) => {
+    setSearch(target.value);
+  };
 
   useEffect(() => {
     dispatch(getCalendarData(user.token));
@@ -15,12 +20,23 @@ const Detail = () => {
   return (
     <div className="container">
       <Header />
+      <nav className="nav">
+        <input
+          className="nav__form-input"
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={handleChangeSearch}
+        />
+      </nav>
       <div className="catalog">
         <h2 className="catalog__title">Catalog</h2>
         <div className="catalog__content">
-          {books.data.map((book) => (
-            <Card book={book} key={book.id} />
-          ))}
+          {books.data
+            .filter((book) => book.title.toLowerCase().includes(search.toLowerCase().trim()))
+            .map((book) => (
+              <Card book={book} key={book.id} />
+            ))}
         </div>
       </div>
     </div>
