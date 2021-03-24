4ef30c28dc7983bdafa371471a62f0ffb276cde1
Khasanov <zkhasanovv@gmail.com>
2021-03-24 14:34:56 +0200

    Added tests for actions

---
+++ src/redux/actions/user.spec.js
@@ -0,0 +1,19 @@
+import { logout, LOGOUT, SET_USER, setUser } from './user';
+
+describe('should user actions work', () => {
+  it('should create an action to set user', () => {
+    const data = 'user';
+    const expectedAction = {
+      type: SET_USER,
+      payload: data,
+    };
+    expect(setUser(data)).toEqual(expectedAction);
+  });
+
+  it('should create an action to logout', () => {
+    const expectedAction = {
+      type: LOGOUT,
+    };
+    expect(logout()).toEqual(expectedAction);
+  });
+});
