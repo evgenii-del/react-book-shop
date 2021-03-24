9053828501651af4f2d86c89661a03c53889fde5
Khasanov <zkhasanovv@gmail.com>
2021-03-24 15:26:38 +0200

    Created tests for reducers

---
+++ src/redux/reducers/catalog.spec.js
@@ -0,0 +1,69 @@
+import catalogReducer from './catalog';
+import { GET_BOOKS_FAILURE, GET_BOOKS_STARTED, GET_BOOKS_SUCCESS } from '../actions/catalog';
+
+describe('catalog reducer', () => {
+  const initialCart = {
+    data: [],
+    isLoading: false,
+    error: null,
+  };
+
+  const books = [
+    {
+      id: '4',
+      title: 'Eloquent Javascript',
+      author: 'Marijn Haverbeke',
+      level: 'Beginner',
+      description:
+        'A book providing an introduction to the JavaScript language and programming in general.',
+      cover: 'https://jsbooks.revolunet.com/img/cover_eloquentjs.png',
+      tags: ['core'],
+      price: 20.1,
+      count: 10,
+    },
+  ];
+
+  const errorMessage = 'error';
+
+  it('should return the initial catalog', () => {
+    expect(catalogReducer(undefined, {})).toEqual(initialCart);
+  });
+
+  it('should change isLoading flag', () => {
+    expect(
+      catalogReducer(initialCart, {
+        type: GET_BOOKS_STARTED,
+      })
+    ).toEqual({
+      data: [],
+      isLoading: true,
+      error: null,
+    });
+  });
+
+  it('should add book data to store', () => {
+    expect(
+      catalogReducer(initialCart, {
+        type: GET_BOOKS_SUCCESS,
+        payload: books,
+      })
+    ).toEqual({
+      data: books,
+      isLoading: false,
+      error: null,
+    });
+  });
+
+  it('should change error', () => {
+    expect(
+      catalogReducer(initialCart, {
+        type: GET_BOOKS_FAILURE,
+        payload: errorMessage,
+      })
+    ).toEqual({
+      data: [],
+      isLoading: false,
+      error: errorMessage,
+    });
+  });
+});
