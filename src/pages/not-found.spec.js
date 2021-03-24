2870e1d306ceb911bc62fbaeb0215f99fd8628ac
Khasanov <zkhasanovv@gmail.com>
2021-03-24 17:25:21 +0200

    Added test for NotFound page

---
+++ src/pages/not-found.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import NotFound from './NotFound';
+
+describe('should render Card component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<NotFound />);
+  });
+
+  it('should contain .not-found wrapper', () => {
+    const wrapper = component.find('.not-found');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should contain title', () => {
+    const title = component.find('.not-found__title');
+    expect(title.text()).toBe('Oops!');
+  });
+});
