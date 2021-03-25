a0bb9a9f37840940266019ea9322c1137e3db364
Khasanov <zkhasanovv@gmail.com>
2021-03-25 18:41:00 +0200

    Changed Detail page.

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 import { Header } from '../components';
 import { addBookToCart } from '../redux/actions/cart';
 
 const Detail = () => {
-  const { books, cart } = useSelector((state) => state);
+  const { cart, user } = useSelector((state) => state);
   const dispatch = useDispatch();
   const history = useHistory();
   const { id } = useParams();
-  const book = books.data.find((item) => item.id === id);
   const isInCart = cart.books.find((item) => item.book.id === id);
-  const { price, title, author, description, tags, cover } = book;
+
+  const [book, setBook] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleAddBookToCart = () => {
     const cartItem = {
@@ -24,38 +26,56 @@ const Detail = () => {
     history.push('/cart');
   };
 
+  useEffect(() => {
+    axios
+      .get(`https://js-band-store-api.glitch.me/books/${id}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      })
+      .then((response) => {
+        setBook(response.data);
+        setIsLoading(false);
+      })
+      .catch(() => {});
+  }, []);
+
   return (
     <div className="container">
       <Header />
       <div className="detail">
         <h2 className="detail__title">Detail</h2>
-        <div className="detail__content">
-          <div className="detail__image-container">
-            <img className="detail__image" src={cover} alt={title} />
-          </div>
-          <div className="detail__info">
-            <h1 className="detail__info-title">{title}</h1>
-            <div className="detail__info-tags">
-              {tags.map((tag) => (
-                <span className="detail__info-tag" key={tag}>
-                  {tag}
-                </span>
-              ))}
+        {isLoading ? (
+          <span>loading...</span>
+        ) : (
+          <div className="detail__content">
+            <div className="detail__image-container">
+              <img className="detail__image" src={book.cover} alt={book.title} />
             </div>
-            <p className="detail__info-author">{author}</p>
-            <p className="detail__info-description">{description}</p>
-            <div className="detail__footer">
-              <p className="detail__info-price">{`Price: ${price}$`}</p>
-              {isInCart ? (
-                <Link to="/cart">The book is already in the cart.</Link>
-              ) : (
-                <button className="detail__btn" type="button" onClick={handleAddBookToCart}>
-                  Add to cart
-                </button>
-              )}
+            <div className="detail__info">
+              <h1 className="detail__info-title">{book.title}</h1>
+              <div className="detail__info-tags">
+                {book.tags.map((tag) => (
+                  <span className="detail__info-tag" key={tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <p className="detail__info-author">{book.author}</p>
+              <p className="detail__info-description">{book.description}</p>
+              <div className="detail__footer">
+                <p className="detail__info-price">{`Price: ${book.price}$`}</p>
+                {isInCart ? (
+                  <Link to="/cart">The book is already in the cart.</Link>
+                ) : (
+                  <button className="detail__btn" type="button" onClick={handleAddBookToCart}>
+                    Add to cart
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
